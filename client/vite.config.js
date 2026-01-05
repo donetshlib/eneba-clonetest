a6578fc75b09a60d03373dbca10403756158d113
donetshlib <donetshlib@gmail.com>
2026-01-05 23:16:36 +0200

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/eneba-clone/",
+  base: "/eneba-clonetest/",
   plugins: [react()],
 });
 
